6cf21ed67dcc61aa91a6392e8c93e3073deeb663
Frank Levering <fr.levering@gmail.com>
2026-07-03 16:43:13 +0200

    fix

--- app/sw.ts
+++ app/sw.ts
@@ -1,6 +1,13 @@
 /// <reference lib="webworker" />
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "serwist";
+
+declare global {
+  interface ServiceWorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
 
 declare const self: ServiceWorkerGlobalScope;
 
